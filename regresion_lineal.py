b4aeb06d549465800ae62a52e4dd0b28fc181d2d
tobiasvidelaug14 <tobiasvidelaug14@gmail.com>
2024-08-05 14:15:16 -0300

    Actualizado el archivo de regresión lineal

--- regresion_lineal.py
+++ regresion_lineal.py
@@ -11,19 +11,19 @@ print(housing.get('DESCR'))
 
 # A partir de los datos de habitaciones promedio y valor promedio de las casas (target)
 
-#X = np.array(housing.get('data')[:,0]) # 'MedInc': median income in block group
+X = np.array(housing.get('data')[:,0]) # 'MedInc': median income in block group
 #X = np.array(housing.get('data')[:,2]) # 'AveRooms': average number of rooms per household
 #X = np.array(housing.get('data')[:,3]) # 'AveBedrms': average number of bedrooms per household
 #X = np.array(housing.get('data')[:,4]) # 'Population': block group population
-#Y = np.array(housing.get('target')) # target: median house value ($100,000)
+Y = np.array(housing.get('target')) # target: median house value ($100,000)
 
-X = np.array([2.86232, 3.8232, 5.862232, 1.2286, 7.3286, 3.124, 2.795, 4.538491])
-Y = np.array([2.53442, 2.8232, 3.26432, 2.1226, 4.6699, 2.3516, 1.2391, 2.6467])
+#X = np.array([2.86232, 3.8232, 5.862232, 1.2286, 7.3286, 3.124, 2.795, 4.538491])
+#Y = np.array([4.53442, 5.8232, 5.26432, 4.1226, 5.2199, 4.3516, 3.2391, 4.6467])
 
 plt.scatter(X, Y)
 
 # añadir columna de 1, para término independiente
-total_filas = 8 # total de filas (instancias) en el conjunto de datos
+total_filas = 20640 # total de filas (instancias) en el conjunto de datos
 X = np.array([np.ones(total_filas), X]).T
 
 # Error Cuadrático Medio (vectorial):
@@ -35,6 +35,8 @@ X = np.array([np.ones(total_filas), X]).T
 
 B = np.linalg.inv(X.T @ X) @ X.T @ Y
 
-plt.plot([0,8], [B[0] + B[1] * 0, B[0] + B[1] * 8], c='red')
+print(B)
+
+plt.plot([0,12], [B[0] + B[1] * 0, B[0] + B[1] * 12], c='red')
 
 plt.show()
